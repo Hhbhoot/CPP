6be018bd35631a651faefb6d0ce8ac1bc3acbb7d
Hhbhoot <hiteshbhoot938@gmail.com>
2023-07-18 16:17:50 +0530

    OOPS

---
+++ REPORT.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class REPORT{
+
+    int adno;
+    char name[20];
+    float arr[5];
+    
+
+    public:
+
+    int READINFO(){
+         
+         cout<<"Enter ad no :";
+         cin>>adno;
+
+         cout<<"Enter name :";
+         cin>>name;
+
+         cout<<"Enter marks of maths science english ss gujarati :";
+         cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+
+
+
+    }
+
+    float avg(){
+
+        return (arr[0]+arr[1]+arr[2]+arr[3]+arr[4])/5;   
+         }
+
+     int DISPLAYINFO(){
+        cout<<"adno\tname\tmaths\tscience\tenglish\tss\tgujarati\taverage"<<"\n";
+        cout<<adno<<"\t"<<name<<"\t"<<arr[0]<<"\t"<<arr[1]<<"\t"<<arr[2]<<"\t"<<arr[3]<<"\t"<<arr[4]<<"\t\t"<<avg();
+     }    
+};
+int main(){
+
+    REPORT r;
+    r.READINFO();
+    float x= r.avg();
+    r.DISPLAYINFO();
+}
